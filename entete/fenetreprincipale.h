467425b09abba69c7ffc123ce6f3b337529a7bad
sebangi <sebastien_angibaud@yahoo.fr>
2018-09-27 17:20:41 +0200

    ajout de widget

--- entete/fenetreprincipale.h
+++ entete/fenetreprincipale.h
@@ -2,6 +2,9 @@
 #define FENETREPRINCIPALE_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QLCDNumber>
+#include <vector>
 
 namespace Ui {
     class FenetrePrincipale;
@@ -16,7 +19,20 @@ class FenetrePrincipale : public QMainWindow
         ~FenetrePrincipale();
 
     private:
-        Ui::FenetrePrincipale *ui;
+        void creer_widgets();
+        void init_widgets();
+
+    private:
+        Ui::FenetrePrincipale *m_ui;
+
+        /** \brief Un pointeur sur le label de l'équipe en cours. */
+        QLabel * m_equipe_en_cours_label;
+
+        /** \brief Un tableau des pointeurs sur les chiffres du chronomètre. */
+        std::vector<QLCDNumber*> m_chronometre_lcd;
+
+        /** \brief Un pointeur sur le label des énigmes de la partie en cours. */
+        QLabel * m_enigmes_en_cours_label;
 };
 
 #endif // FENETREPRINCIPALE_H
--- src/fenetreprincipale.cpp
+++ src/fenetreprincipale.cpp
@@ -3,12 +3,94 @@
 
 FenetrePrincipale::FenetrePrincipale(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::FenetrePrincipale)
+    m_ui(new Ui::FenetrePrincipale)
 {
-    ui->setupUi(this);
+    m_ui->setupUi(this);
+
+    creer_widgets();
+    init_widgets();
+
+    resize(QSize(1200,900));
 }
 
 FenetrePrincipale::~FenetrePrincipale()
 {
-    delete ui;
+    delete m_ui;
+}
+
+
+/** --------------------------------------------------------------------------------------
+ \brief Création des différents widgets.
+*/
+void FenetrePrincipale::creer_widgets()
+{
+
+}
+
+/** --------------------------------------------------------------------------------------
+ \brief Initialisation des différents widgets.
+*/
+void FenetrePrincipale::init_widgets()
+{
+    QStyle* style = QApplication::style();
+
+    setWindowTitle("Serveur ESCAPE BTS");
+
+    // ZONE PARTIE EN COURS
+    QWidget * zone_partie_en_cours = new QWidget(this);
+    QVBoxLayout * zone_partie_en_cours_lay = new QVBoxLayout();
+    zone_partie_en_cours_lay->setMargin(10);
+    zone_partie_en_cours_lay->setSpacing(10);
+
+    QLabel * partie_en_cours_label = new QLabel( "PARTIE EN COURS" );
+    partie_en_cours_label->setObjectName("TitreSection");
+    partie_en_cours_label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    partie_en_cours_label->setAlignment(Qt::AlignCenter | Qt::AlignVCenter);
+    zone_partie_en_cours_lay->addWidget(partie_en_cours_label);
+
+    QWidget * zone_equipe_en_cours = new QWidget(this);
+    QHBoxLayout * zone_equipe_en_cours_lay = new QHBoxLayout();
+    zone_equipe_en_cours_lay->setMargin(10);
+    zone_equipe_en_cours_lay->setSpacing(10);
+
+    m_equipe_en_cours_label = new QLabel( "Equipe non renseignée" );
+    m_equipe_en_cours_label->setObjectName("NomEquipe");
+    m_equipe_en_cours_label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    m_equipe_en_cours_label->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+    zone_equipe_en_cours_lay->addWidget(m_equipe_en_cours_label);
+
+    m_chronometre_lcd.resize(8);
+    for ( unsigned int i = 0; i != 8; ++i )
+    {
+        if ( i == 2 || i == 6 )
+        {
+            QLabel * deux_points_label = new QLabel( ":" );
+            zone_equipe_en_cours_lay->addWidget( deux_points_label);
+        }
+        else if ( i == 4 )
+        {
+            QLabel * deux_points_label = new QLabel( " / " );
+            zone_equipe_en_cours_lay->addWidget( deux_points_label);
+        }
+
+        m_chronometre_lcd[i] = new QLCDNumber();
+        zone_equipe_en_cours_lay->addWidget( m_chronometre_lcd[i]);
+    }
+
+    m_enigmes_en_cours_label = new QLabel( "Énigmes : 0 / 0" );
+    m_enigmes_en_cours_label->setObjectName("EnigmesEnCours");
+    m_enigmes_en_cours_label->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    m_enigmes_en_cours_label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+    zone_equipe_en_cours_lay->addWidget(m_enigmes_en_cours_label);
+
+    zone_equipe_en_cours->setLayout(zone_equipe_en_cours_lay);
+
+    zone_partie_en_cours_lay->addWidget(zone_equipe_en_cours);
+    zone_partie_en_cours->setLayout(zone_partie_en_cours_lay);
+
+    m_ui->centralLayout->setMargin(0);
+    m_ui->centralLayout->setSpacing(0);
+
+    m_ui->centralLayout->addWidget(zone_partie_en_cours,1);
+    //m_ui->centralLayout->addWidget(s_vue_logs, 5);
 }
